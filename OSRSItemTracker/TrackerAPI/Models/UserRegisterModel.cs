f068a7d9a6984c189db11e7fcbd9a8562ed263d8
Tye <tyewalker09@gmail.com>
2022-07-03 14:35:53 +0200

    models

---
+++ OSRSItemTracker/TrackerAPI/Models/UserRegisterModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TrackerAPI.Models
+{
+    public class UserRegisterModel
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
