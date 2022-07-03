742c163731712e2b88e2735381206cdfd17b157e
Tye <tyewalker09@gmail.com>
2022-07-03 12:13:29 +0200

    Volume Service

---
+++ OSRSItemTracker/TrackerAPI/Services/IVolumeService.cs
@@ -0,0 +1,17 @@
+﻿using TrackerAPI.Entities;
+
+namespace TrackerAPI.Services
+{
+    public interface IVolumeService
+    {
+        Task<VolumeEntity> GetVolumeEntity(long id);
+        Task<VolumeEntity> GetLastHourVolumeForItem(long itemId);
+        Task<VolumeEntity> GetLastDayVolumeForItem(long itemId);
+        Task<VolumeEntity> Create(VolumeEntity model);
+        Task<VolumeEntity> Update(VolumeEntity model);
+        Task<VolumeEntity> DeleteById(long id);
+        Task<List<VolumeEntity>> GetLastHourForAllItems();
+        Task<List<VolumeEntity>> CreateBatch(List<VolumeEntity> models);
+        Task<VolumeEntity> VerifyVolumeDoesNotExist(VolumeEntity volumeEntity);
+    }
+}
