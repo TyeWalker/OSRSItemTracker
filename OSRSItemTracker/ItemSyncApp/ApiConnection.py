d8f7aaedf5f1de7ec02bf606e0debc9a2c601f5b
Tye <tyewalker09@gmail.com>
2022-07-03 12:27:55 +0200

    api connections

---
+++ OSRSItemTracker/ItemSyncApp/ApiConnection.py
@@ -0,0 +1,38 @@
+import requests
+
+
+# OSRS url
+osrsurl = "https://prices.runescape.wiki/api/v1/osrs/"
+useragent = "Item Set Price Tracker - TFunk#7503"
+
+
+# Routes:
+# latest
+# mapping
+# 1h
+# 24h
+
+
+def fetchrequest(route):
+    url = osrsurl + route
+    response = requests.get(url, headers={'User-Agent': useragent}).json()
+    return response
+
+
+# API connections
+apiurl = "https://localhost:7174/"
+headers = {
+    'Content-type':'application/json',
+    'Accept':'application/json'
+}
+
+def fetchfromapi(route):
+    url = apiurl + route
+    response = requests.get(url, headers={'User-Agent': useragent}, verify=False).json()
+    return response
+
+
+def posttoapi(route, requestbody):
+    url = apiurl + route
+    response = requests.post(url, data=requestbody, verify=False, headers=headers)
+    return response
