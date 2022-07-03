36d029fcf102033913c4b6673e39abb667a2583d
Tye <tyewalker09@gmail.com>
2022-07-03 14:39:26 +0200

    model

---
+++ OSRSItemTracker/TrackerAPI/Models/UserAuthModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TrackerAPI.Models
+{
+    public class UserAuthModel
+    {
+        public string UserName { get; set; } = string.Empty;
+        public byte[] Password { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
