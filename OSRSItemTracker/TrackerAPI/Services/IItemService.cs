8d79c5794a5fa970f08c4ddf44f02930f27046fe
Tye <tyewalker09@gmail.com>
2022-07-03 12:10:17 +0200

    IItemService

--- OSRSItemTracker/TrackerAPI/Services/IItemService.cs
+++ OSRSItemTracker/TrackerAPI/Services/IItemService.cs
@@ -2,5 +2,12 @@
 {
     public interface IItemService
     {
+        Task<List<ItemEntity>> GetAll();
+        Task<ItemEntity> GetByItemId(long itemId);
+        Task<ItemEntity> GetById(long id);
+        Task<ItemEntity> Create(ItemEntity model);
+        Task<ItemEntity> Update(ItemEntity model);
+        Task<ItemEntity> DeleteById(long id);
+        Task<List<ItemEntity>> CreateBatch(List<ItemEntity> items);
     }
 }
