522042d8ac496147c8591dc8bfd45d9b01e83eae
Tye <tyewalker09@gmail.com>
2022-07-03 12:20:56 +0200

    register services

--- OSRSItemTracker/TrackerAPI/Program.cs
+++ OSRSItemTracker/TrackerAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using TrackerAPI.Data;
+using TrackerAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,10 @@ builder.Services.AddDbContext<DataContext>(options =>
 
 
 builder.Services.AddControllers();
+// Register services
+builder.Services.AddScoped<IItemService, ItemService>();
+builder.Services.AddScoped<IPriceService, PriceService>();
+builder.Services.AddScoped<IVolumeService, VolumeService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
