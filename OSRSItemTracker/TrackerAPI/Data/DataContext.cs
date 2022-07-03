0357d3cfa57cdc0db862f8da2730283405c8d59b
Tye <tyewalker09@gmail.com>
2022-07-03 11:50:13 +0200

    data context

---
+++ OSRSItemTracker/TrackerAPI/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrackerAPI.Entities;
+
+namespace TrackerAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<ItemEntity> Items { get; set; }
+        public DbSet<PriceEntity> LatestPrices { get; set; }
+        public DbSet<VolumeEntity> Volume { get; set; }
+    }
+}
