6707358738f7581c63fcc2c0803cdb5ae9a2493f
Tye <tyewalker09@gmail.com>
2022-07-03 12:11:57 +0200

    Price Service

---
+++ OSRSItemTracker/TrackerAPI/Services/IPriceService.cs
@@ -0,0 +1,15 @@
+﻿using TrackerAPI.Entities;
+
+namespace TrackerAPI.Services
+{
+    public interface IPriceService
+    {
+        Task<List<PriceEntity>> GetAll();
+        Task<PriceEntity> GetByItemId(long itemId);
+        Task<PriceEntity> GetById(long Id);
+        Task<PriceEntity> Create(PriceEntity latestPrice);
+        Task<PriceEntity> Update(PriceEntity latestPrice);
+        Task<PriceEntity> DeleteById(long id);
+        Task<PriceEntity> VerifyPriceDoesNotExist(PriceEntity newPrice);
+    }
+}
